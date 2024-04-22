84bedf424541b26a73e435ff520b2e3f372bdf7a
riddhidhanani <87064012+riddhidhanani@users.noreply.github.com>
2024-04-22 01:25:56 -0500

    Add files via upload

---
+++ kruskal.py
@@ -0,0 +1,18 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def dfs(graph, node, visited):
+    if node not in visited:
+        print(node, end=' ')
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+
+# Start DFS from node 'A'
+dfs(graph, 'A', set())
